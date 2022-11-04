f7cd1f254f3068ec0d9cf59612445ea972fa2a2e
Trent Schafer <trent.schafer@gmail.com>
2022-11-04 12:16:23 +1100

    Use _CONNECT_IDENTIFIER for prompt

--- login.sql
+++ login.sql
@@ -32,7 +32,16 @@ with instance_info as (
         global_name
 )
 select
-    substr(global_name, 1, decode(dot_index, 0, length(global_name), dot_index-1))
+    -- 04/11/2022: Switch to builtin for connection identifier to get a better
+    -- value for ldap based connections (_CONNECT_IDENTIFIER)
+    -- Found example from here: https://asktom.oracle.com/pls/apex/f?p=100:11:0::::P11_QUESTION_ID:446220075876
+    -- Keeping as SQL query so the username can be kept in lowercase (instead of _USER
+    -- which will be upper-case).
+    -- If it has a dot in the name, use the first part of the string instead
+    case
+        when dot_index > 0 then substr(global_name, 1, decode(dot_index, 0, length(global_name), dot_index-1))
+        else '_CONNECT_IDENTIFIER'
+    end
     || ': '
     || lower(user)
     global_name
